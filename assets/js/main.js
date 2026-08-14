c447bb287fd1cda4aeea964a78a9b5ebfae39515
Ali-Najar <anajar13750@gmail.com>
2026-08-14 09:21:19 +0200

    feat: add mobile menu interaction

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,11 @@ const themeButton = document.getElementById("themeButton");
 themeButton.addEventListener("click", function () {
     document.body.classList.toggle("dark-theme");
 });
+
+
+const menuButton = document.getElementById("menuButton");
+const navLinks = document.querySelector(".nav-links");
+
+menuButton.addEventListener("click", function () {
+    navLinks.classList.toggle("active");
+});
